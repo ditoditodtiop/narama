1cdb15ba1a9bd2c4f8e9fba51a910d4c2492d611
narma1 <dito.narama@gmail.com>
2025-06-11 19:14:37 +0400

    first commit

--- posts/admin.py
+++ posts/admin.py
@@ -6,7 +6,6 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'visibility', 'created_at')
     search_fields = ('title', 'author__username')
     list_filter = ('visibility', 'created_at')
-    readonly_fields = ('created', 'modified')
 
 @admin.register(PostReaction)
 class PostReactionAdmin(admin.ModelAdmin):
